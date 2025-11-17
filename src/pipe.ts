78b0cd3d8d8165e9e2963a782beed7833060ba19
TechnoJo4 <technojo4@gmail.com>
2025-11-17 16:28:08 -0500

    a

--- src/pipe.ts
+++ src/pipe.ts
@@ -18,5 +18,5 @@ declare global {
   }
 }
 
-Object.defineProperty(Object.prototype, "tap", { get: () => tap, set: () => {}, enumerable: false });
-Object.defineProperty(Object.prototype, "pipe", { get: () => pipe, set: () => {}, enumerable: false });
+Object.defineProperty(Object.prototype, "tap", { get(){tap}, set(){}, enumerable: false });
+Object.defineProperty(Object.prototype, "pipe", { get(){pipe}, set(){}, enumerable: false });
